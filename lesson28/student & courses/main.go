61b6401a01346c7d5134e27c6b837f9b6a8c7871
Azizbek <146860466+pronium@users.noreply.github.com>
2024-05-30 04:38:03 -0700

    Task done!

---
+++ lesson28/student & courses/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	// "github.com/Go11Group/at_lesson/lesson28/model"
+	"github.com/Go11Group/at_lesson/lesson28/storage/postgres"
+)
+
+func main() {
+
+	db, err := postgres.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	//Students
+	st := postgres.NewStudentRepo(db)
+	students, err := st.GetAllStudents()
+	if err != nil {
+		panic(err)
+	}
+
+	student, _ := st.GetByID(students[2].ID)
+	for x := range students {
+		fmt.Println(students[x])
+	}
+
+	fmt.Println(student, "\n")
+	//Courses
+	cr := postgres.NewCourseRepo(db)
+	kurslar, err := cr.GetAllCourses()
+	if err != nil {
+		panic(err)
+	}
+
+	kurs, _ := cr.GetByID(kurslar[2].ID)
+	for x := range kurslar {
+		fmt.Println(kurslar[x])
+	}
+
+	fmt.Println(kurs)
+}
