954a5f66d6f561afe0bbd86bcef9f230ac9e49e0
Azizbek <146860466+pronium@users.noreply.github.com>
2024-06-27 18:33:26 +0500

    Task done

--- lesson46/WeatherAndBusService/server/Services/server.go
+++ lesson46/WeatherAndBusService/server/Services/server.go
@@ -1,16 +1,14 @@
 package Services
 
-import pb "WeatherAndBusService/server/genproto/BusService"
+import (
+	pb "WeatherAndBusService/server/genproto/BusService"
+	pw "WeatherAndBusService/server/genproto/WeatherService"
+)
 
 type BusServer struct {
 	pb.UnimplementedTransportServiceServer
 }
 
 type WeatherServer struct {
-	pb.UnimplementedTransportServiceServer
-}
-
-func (s *WeatherServer) mustEmbedUnimplementedWeatherServiceServer() {
-	//TODO implement me
-	panic("implement me")
+	pw.UnimplementedWeatherServiceServer
 }
